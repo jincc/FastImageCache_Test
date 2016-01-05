a91d905b584ebcaac3c30c2367376106df96479d
jiangjunlong <junlongj@jumei.com>
2016-01-05 22:30:29 +0800

    Initial Commit

---
+++ IRanderTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IRanderTest
+//
+//  Created by junlongj on 16/1/5.
+//  Copyright © 2016年 junlongj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
